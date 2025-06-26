3a626745025ed8b6b3e666a6e72f65608fdc2137
Vitor Moraes <vitordemoraesfilho@gmail.com>
2025-06-26 10:45:14 -0300

    adding global root css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
