6c3e7f2cda73fd9f7b1ed414e4aed1413c2c788c
Luciano Chiodini <138342951+yLucino@users.noreply.github.com>
2025-06-29 19:30:46 -0300

    adding contacts page no CSS

--- src/app/Components/agility-practicality-pop-up/agility-practicality-pop-up.component.ts
+++ src/app/Components/agility-practicality-pop-up/agility-practicality-pop-up.component.ts
@@ -8,11 +8,11 @@ import { trigger, transition, style, animate, AnimationEvent } from '@angular/an
   animations: [
     trigger('fadeInOut', [
       transition(':enter', [
-        style({ opacity: 0, transform: 'scale(0.95)' }),
+        style({ opacity: 0, transform: 'scale(0.55)' }),
         animate('200ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
       ]),
       transition(':leave', [
-        animate('200ms ease-in', style({ opacity: 0, transform: 'scale(0.95)' }))
+        animate('200ms ease-in', style({ opacity: 0, transform: 'scale(0.55)' }))
       ]),
     ])
   ]
