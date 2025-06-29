41a63f2c1333009421333e5bac5bae1f8c934f6a
Luciano Chiodini <138342951+yLucino@users.noreply.github.com>
2025-06-29 14:02:57 -0300

    adding PopUp AgilityAndPracticality in services page with animations

---
+++ src/app/Components/agility-practicality-pop-up/agility-practicality-pop-up.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { trigger, transition, style, animate, AnimationEvent } from '@angular/animations';
+
+@Component({
+  selector: 'app-agility-practicality-pop-up',
+  templateUrl: './agility-practicality-pop-up.component.html',
+  styleUrls: ['./agility-practicality-pop-up.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0.95)' }),
+        animate('200ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ opacity: 0, transform: 'scale(0.95)' }))
+      ]),
+    ])
+  ]
+})
+export class AgilityPracticalityPopUpComponent {
+@Output() close = new EventEmitter<void>();
+  visible = true;
+
+  closePopup() {
+    this.visible = false;
+  }
+
+  onAnimationDone(event: AnimationEvent) {
+    if (event.toState === 'void') {
+      this.close.emit();
+    }
+  }
+}
--- src/app/Pages/main/services/services.component.ts
+++ src/app/Pages/main/services/services.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
+  showAgilityPopUp = false;
 
+  openPopup() {
+    this.showAgilityPopUp = true;
+  }
+
+  closePopup() {
+    this.showAgilityPopUp = false;
+  }
 }
