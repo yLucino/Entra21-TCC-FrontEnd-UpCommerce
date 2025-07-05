28051114143b72cc4aaa92f8a99be14030f9f0c3
Luciano Chiodini <138342951+yLucino@users.noreply.github.com>
2025-07-05 17:05:38 -0300

    adding suport plan PopUp with CSS and Ts Logic

--- src/app/Components/plan-suport/plan-suport.component.ts
+++ src/app/Components/plan-suport/plan-suport.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { trigger, transition, style, animate, AnimationEvent } from '@angular/animations';
 
 @Component({
   selector: 'app-plan-suport',
   templateUrl: './plan-suport.component.html',
-  styleUrls: ['./plan-suport.component.css']
+  styleUrls: ['./plan-suport.component.css'],
+  animations: [
+      trigger('fadeInOut', [
+        transition(':enter', [
+          style({ opacity: 0, transform: 'scale(0.55)' }),
+          animate('200ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
+        ]),
+        transition(':leave', [
+          animate('200ms ease-in', style({ opacity: 0, transform: 'scale(0.55)' }))
+        ]),
+      ])
+    ]
 })
 export class PlanSuportComponent {
+  @Input() username: string | null = null;
+  @Input() email: string | null = null;
+  @Output() closePlan = new EventEmitter<void>();
 
+  visible = true;
+
+  closeComponent() {
+    this.visible = false;
+  }
+
+  onAnimationDone(event: AnimationEvent) {
+    if (event.toState === 'void') {
+      this.closePlan.emit();
+    }
+  }
 }
