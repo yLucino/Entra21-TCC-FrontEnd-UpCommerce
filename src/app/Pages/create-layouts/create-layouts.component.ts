319e6f62063edc8a53c45cac446bfd340e94f7f1
Luciano Chiodini <138342951+yLucino@users.noreply.github.com>
2025-07-07 11:43:49 -0300

    adding nav-bar-workshop in create-layouts page

---
+++ src/app/Pages/create-layouts/create-layouts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-layouts',
+  templateUrl: './create-layouts.component.html',
+  styleUrls: ['./create-layouts.component.css']
+})
+export class CreateLayoutsComponent {
+
+}
