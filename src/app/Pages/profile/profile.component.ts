3baa6ea99cd630614d5e4ab045b1bffce03b0306
Luciano Chiodini <138342951+yLucino@users.noreply.github.com>
2025-08-21 22:39:26 -0300

    adding project sevice to comunication with backend, add logic to get/add in profile and projects-workshop

--- src/app/Pages/profile/profile.component.ts
+++ src/app/Pages/profile/profile.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ProjectInterface } from 'src/app/interfaces/project.interface';
 import { UserInterface, UserTokenInterface } from 'src/app/interfaces/user.interface';
 import { AuthService } from 'src/app/services/auth.service';
+import { ProjectService } from 'src/app/services/project.service';
 import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-profile',
@@ -12,15 +14,10 @@ import { UserService } from 'src/app/services/user.service';
 export class ProfileComponent implements OnInit {
   userId!: number;
   user!: UserInterface;
-  project: ProjectInterface = {
-    title: '',
-    description: '',
-    subTitle: '',
-    urlLogo: '',
-  };
+  projects: ProjectInterface[] = [];
   visible: boolean = false;
 
-  constructor(private authService: AuthService, private userService: UserService) {}
+  constructor(private authService: AuthService, private userService: UserService, private projectService: ProjectService) {}
 
   ngOnInit() {
     this.authService.user$.subscribe((data: UserTokenInterface | null) => {
@@ -32,6 +29,24 @@ export class ProfileComponent implements OnInit {
     this.userService.getUserById(this.userId).subscribe((user: UserInterface) => {
       if (user) {
         this.user = user;
+      }
+    });
+
+    this.projectService.getAllProjectByUserId(this.userId).subscribe({
+      next: (res) => {
+        this.projects = res;
+      },
+      error: (err) => {
+        Swal.fire({
+          toast: true,            
+          position: 'top-end',    
+          icon: 'error',    
+          title: 'Erro ao carregar os projetos!',
+          showConfirmButton: false,
+          timer: 3000, 
+          timerProgressBar: true
+        });
+        console.log(err);
       }
     });
   }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,6 +1,31 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { ProjectInterface } from "../interfaces/project.interface";
+import { environment } from "src/environments/environment.development";
+import { Observable } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class ProjectService {
 
+  constructor(private http: HttpClient) {}
+  
+  getAllProjectByUserId(userId: number): Observable<ProjectInterface[]> {
+    return this.http.get<ProjectInterface[]>(`${environment.apiUrl}/Project/user/${userId}`);
+  }
+
+  getProjectById(userId: number, id: number): Observable<ProjectInterface> {
+    return this.http.get<ProjectInterface>(`${environment.apiUrl}/Project/user/${userId}/${id}`);
+  }
+
+  postProject(userId: number, data: ProjectInterface): Observable<ProjectInterface> {
+    return this.http.post<ProjectInterface>(`${environment.apiUrl}/Project/user/${userId}`, data);
+  }
+
+  putProject(userId: number, id: number, data: ProjectInterface): Observable<ProjectInterface> {
+    return this.http.post<ProjectInterface>(`${environment.apiUrl}/Project/user/${userId}/${id}`, data);
+  }
+
+  deleteProject(userId: number, id: number): Observable<ProjectInterface> {
+    return this.http.delete<ProjectInterface>(`${environment.apiUrl}/Project/user/${userId}/${id}`);
+  }
 }
